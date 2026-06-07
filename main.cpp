8b0d9572f9aa4575d3b4e682747b0e1df2d4dba2
EM1S <radmixiaomi8t@gmail.com>
2026-06-07 13:53:22 +0300

    Create main.cpp
    Add source code for variant 8

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*Платоненко Владислав Денисович 1 УТС Вариант 8 */
+#include <iostream>
+#include <cmath>
+#include <clocale>
+using namespace std;
+double f(double x) {
+	return x * x;
+}
+int main() {
+	setlocale(LC_CTYPE, "rus");
+	double x, dX;
+	while (true) {
+		cout << "Введите число Х =  ";
+		if (cin >> x) {
+			break;
+		}
+		else {
+			cout << "Введено не число" << endl;
+		}
+	}
+	while (true) {
+		cout << "Введите ненулевое число dX = ";
+		if (cin >> dX && dX != 0) {
+			break;
+		}
+		else {
+			cout << "dX должно быть числом, но не 0" << endl;
+		}
+	}
+	double Pravaya_formula = (f(x + dX) - f(x)) / dX;
+	double Centralnaya_formula = (f(x + dX) - f(x - dX)) / (2 * dX);
+	double Levaya_formula = (f(x) - f(x - dX)) / dX;
+	cout << "Правая разностная производная = " << Pravaya_formula << endl;
+	cout << "Центральная разностная производная = " << Centralnaya_formula << endl;
+	cout << "Левая разностная производная = " << Levaya_formula << endl;
+	return 0;
+}
